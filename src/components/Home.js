906618f22f71f106c1c901625ea20eb618f7eeb9
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2021-09-18 20:34:31 -0400

    added links to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,23 +13,29 @@ const Home = () => {
 
   const NameBlock = () => {
     return (
-      <div id="name-block">
-        <h1 id="first-name">LUTHER</h1>
-        <h1 id="middle-name">CALVIN</h1>
-        <h1 id="last-name">RIGGS</h1>
-        <h2 id="job-title">Frontend Web Developer</h2>
-      </div>
+      <main className="jumbotron">
+        <a href="https://www.linkedin.com/in/lcriggs" target="_blank" rel="noreferrer">
+          <div id="name-block">
+            <h1 id="first-name">LUTHER</h1>
+            <h1 id="middle-name">CALVIN</h1>
+            <h1 id="last-name">RIGGS</h1>
+            <h2 id="job-title">Frontend Web Developer</h2>
+          </div>
+        </a>
+      </main>
     )
   }
 
   const HeadShot = () => {
     return (
       <div id="profile-image-block">
-        <img 
-          id="profile-image" 
-          src="/images/profile-image4a.jpg" 
-          alt="Profile"
-        />
+        <a href="https://www.linkedin.com/in/lcriggs" target="_blank" rel="noreferrer">
+          <img 
+            id="profile-image" 
+            src="/images/profile-image4a.jpg" 
+            alt="Profile"
+          />
+        </a>
       </div>
     )
   }
